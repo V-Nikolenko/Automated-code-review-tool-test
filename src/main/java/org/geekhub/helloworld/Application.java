b4b97a6ba619429b1cbd7a3bd5cc917401587090
Vladyslav Nikolenko <vladyslav.nikolenko@pitchbook.com>
2023-07-16 18:10:41 +0300

    Added name to build step

--- src/main/java/org/geekhub/helloworld/Application.java
+++ src/main/java/org/geekhub/helloworld/Application.java
@@ -2,7 +2,13 @@ package org.geekhub.helloworld;
 
 public class Application {
 
+    static String s = "Hello world";
+
+//    Useless comment
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(s);
     }
+
+
 }
