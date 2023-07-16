b8d052ad1258094432e118dfe0b2976aac7d14df
Vladyslav Nikolenko <vladyslav.nikolenko@pitchbook.com>
2023-07-16 20:39:00 +0300

    Made code ugly

--- src/main/java/org/geekhub/helloworld/Application.java
+++ src/main/java/org/geekhub/helloworld/Application.java
@@ -2,12 +2,10 @@ package org.geekhub.helloworld;
 
 public class Application {
 
-    static String s = "Hello world";
-
 //    Useless comment
 
     public static void main(String[] args) {
-        System.out.println(s);
+        System.out.println(UsellllessService.returnHelloWorldMessage());
     }
 
 
---
+++ src/main/java/org/geekhub/helloworld/UsellllessService.java
@@ -0,0 +1,16 @@
+package org.geekhub.helloworld;
+
+public class UsellllessService {
+
+    private final String unusedField = "";
+
+    public UsellllessService() {
+
+    }
+
+    static String s = "Hello World!";
+
+    public static String returnHelloWorldMessage() {
+        return s;
+    }
+}
